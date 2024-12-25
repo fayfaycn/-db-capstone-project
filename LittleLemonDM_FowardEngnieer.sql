276a5adc6880d6dd6242f35d7251b9914ceca4bb
fayfaycn <79063835+fayfaycn@users.noreply.github.com>
2024-12-25 16:02:28 -0600

    Update LittleLemonDM_FowardEngnieer.sql
    Undated the schema name to LittleLemonDB

--- LittleLemonDM_FowardEngnieer.sql
+++ LittleLemonDM_FowardEngnieer.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema LLDB
+-- Schema LittleLemonDB
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema LLDB
+-- Schema LittleLemonDB
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `LLDB` DEFAULT CHARACTER SET utf8 ;
-USE `LLDB` ;
+CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8 ;
+USE `LittleLemonDB` ;
 
 -- -----------------------------------------------------
--- Table `LLDB`.`Customers`
+-- Table `LittleLemonDB`.`Customers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LLDB`.`Customers` (
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Customers` (
   `CustomerID` INT NOT NULL AUTO_INCREMENT,
   `FullName` VARCHAR(100) NOT NULL,
   `ContactNumber` INT NOT NULL,
@@ -27,9 +27,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LLDB`.`Bookings`
+-- Table `LittleLemonDB`.`Bookings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LLDB`.`Bookings` (
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Bookings` (
   `BookingID` INT NOT NULL AUTO_INCREMENT,
   `TableNumber` INT NULL,
   `BookingDate` DATETIME NULL,
@@ -38,16 +38,16 @@ CREATE TABLE IF NOT EXISTS `LLDB`.`Bookings` (
   INDEX `CustomerID_fk_idx` (`CustomerID` ASC) VISIBLE,
   CONSTRAINT `CustomerID_fk`
     FOREIGN KEY (`CustomerID`)
-    REFERENCES `LLDB`.`Customers` (`CustomerID`)
+    REFERENCES `LittleLemonDB`.`Customers` (`CustomerID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LLDB`.`MenuItem`
+-- Table `LittleLemonDB`.`MenuItem`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LLDB`.`MenuItem` (
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItem` (
   `ItemID` INT NOT NULL AUTO_INCREMENT,
   `CourseName` VARCHAR(100) NOT NULL,
   `StarterName` VARCHAR(100) NOT NULL,
@@ -57,9 +57,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LLDB`.`Menus`
+-- Table `LittleLemonDB`.`Menus`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LLDB`.`Menus` (
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menus` (
   `MenuID` INT NOT NULL AUTO_INCREMENT,
   `ItemID` INT NOT NULL,
   `MenuName` VARCHAR(100) NOT NULL,
@@ -69,16 +69,16 @@ CREATE TABLE IF NOT EXISTS `LLDB`.`Menus` (
   INDEX `ItemID_idx` (`ItemID` ASC) VISIBLE,
   CONSTRAINT `ItemID`
     FOREIGN KEY (`ItemID`)
-    REFERENCES `LLDB`.`MenuItem` (`ItemID`)
+    REFERENCES `LittleLemonDB`.`MenuItem` (`ItemID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LLDB`.`Orders`
+-- Table `LittleLemonDB`.`Orders`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LLDB`.`Orders` (
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
   `OrderID` INT NOT NULL AUTO_INCREMENT,
   `MenuID` INT NOT NULL,
   `CustomerID` INT NOT NULL,
@@ -90,21 +90,21 @@ CREATE TABLE IF NOT EXISTS `LLDB`.`Orders` (
   INDEX `CustomerID_idx` (`CustomerID` ASC) VISIBLE,
   CONSTRAINT `MenuID`
     FOREIGN KEY (`MenuID`)
-    REFERENCES `LLDB`.`Menus` (`MenuID`)
+    REFERENCES `LittleLemonDB`.`Menus` (`MenuID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `CustomerID`
     FOREIGN KEY (`CustomerID`)
-    REFERENCES `LLDB`.`Customers` (`CustomerID`)
+    REFERENCES `LittleLemonDB`.`Customers` (`CustomerID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LLDB`.`DeliveryStatus`
+-- Table `LittleLemonDB`.`DeliveryStatus`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LLDB`.`DeliveryStatus` (
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`DeliveryStatus` (
   `DeliveryID` INT NOT NULL AUTO_INCREMENT,
   `OrderID` INT NOT NULL,
   `Status` VARCHAR(100) NOT NULL,
@@ -113,16 +113,10 @@ CREATE TABLE IF NOT EXISTS `LLDB`.`DeliveryStatus` (
   INDEX `OrderID_idx` (`OrderID` ASC) VISIBLE,
   CONSTRAINT `OrderID`
     FOREIGN KEY (`OrderID`)
-    REFERENCES `LLDB`.`Orders` (`OrderID`)
+    REFERENCES `LittleLemonDB`.`Orders` (`OrderID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
